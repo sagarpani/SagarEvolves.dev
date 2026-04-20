772b1b5d44afae39bf3738420cb927353540bb39
Sagar Pani <sagarpani22@gmail.com>
2026-04-21 03:12:30 +0530

    Scroll position reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer/Footer"
 import Navbar from "./components/Navbar/Navbar"
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop"
 import { Outlet } from "react-router"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
